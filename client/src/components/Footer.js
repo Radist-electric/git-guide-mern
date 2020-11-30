89cf2d66dcff75fc9a0aef47b20ab02d6dde30c2
Radist <radist-electric@yandex.ru>
2020-12-01 00:21:57 +0500

    I changed the footer using the Material-UI

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,23 @@
-import react from 'react'
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    zIndex: 1201,
+    marginTop: 'auto',
+    color: '#ffffff'
+  }
+}))
 
 export const Footer = () => {
+  const classes = useStyles()
   return (
-    <div className="blue darken-4 footer">
-      <div className="container white-text">
-        <h4>2020 &#169; Путеводитель по Git</h4>
-        <p>Генерирует команды Git</p>
-      </div>
-    </div>
+    <Box className={classes.root} bgcolor="primary.main">
+      <Container>
+      <h4>2020 &#169; Путеводитель по Git</h4>
+      <p>Генерирует команды Git</p>
+      </Container>
+    </Box>
   )
 }

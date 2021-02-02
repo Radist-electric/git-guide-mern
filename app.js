0dd7508c1ab97c6968a054772045ae6260fc7f6e
Radist <radist-electric@yandex.ru>
2021-02-02 23:12:42 +0500

    I prepared my application for deploy to Heroku

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
-app.use(express.json({extented: true}))
+app.use(express.json({ extented: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 
-const PORT = config.get('port') || 5000
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || config.get('port') || 5000
 
 async function start() {
-  try{
+  try {
     await mongoose.connect(config.get('mongoUri'), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     })
-    app.listen(PORT, () => {console.log(`App has been started at port ${PORT}...`)})
-  } catch(e) {
+    app.listen(PORT, () => { console.log(`App has been started at port ${PORT}...`) })
+  } catch (e) {
     console.log('Server error', e.message)
     process.exit(1)
   }
